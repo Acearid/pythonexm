30d80fc45564a5a887437d15ec489f2048591639
Acearid <noel.dansell@gmail.com>
2022-02-09 17:17:50 +0100

    added description

--- koden/medel.py
+++ koden/medel.py
@@ -1,10 +1,15 @@
+#listan nummmer
 num = [1,50,123,13,15,16]
 
+#funktionen 
 def medelvarde(lista):
+    #variablarna
     summa=0
     langd=0
+    #loopen
     for i in lista:
         summa += i
         langd += 1
     return summa/langd
+#svaret
 print(medelvarde(num))
